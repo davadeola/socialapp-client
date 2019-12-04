0cc085f26b1fa4376b49025bafb025c4634ea6fc
davadeola <adeoladavid15@gmail.com>
2019-12-04 14:45:05 +0300

    Setup app client

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+//MUI 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar'
+
+export class Navbar extends Component {
+    render() {
+        return (
+            <AppBar position='fixed'>
+                
+            </AppBar>
+        )
+    }
+}
+
+export default Navbar
